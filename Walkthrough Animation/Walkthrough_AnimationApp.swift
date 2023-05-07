1dffe57a04d3957050eb0eda3ff413a3963bdc60
Amish Tufail <amish.tufail2002@gmail.com>
2023-05-07 17:50:49 +0500

    Initial Commit

---
+++ Walkthrough Animation/Walkthrough_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Walkthrough_AnimationApp.swift
+//  Walkthrough Animation
+//
+//  Created by Amish Tufail on 07/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Walkthrough_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
